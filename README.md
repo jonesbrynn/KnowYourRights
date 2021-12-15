bb3534ed8cfe81b8874a66561bc797896e9d9452
jonesbrynn <jonesbrynn1@yahoo.com>
2021-12-15 14:01:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-neiwwh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-neiwwh)
---
+++ src/national-law/national-law.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-national-law',
+  templateUrl: './national-law.component.html',
+  styleUrls: ['./national-law.component.css']
+})
+export class NationalLawComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
