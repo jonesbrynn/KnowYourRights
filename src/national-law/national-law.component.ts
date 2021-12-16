bf14d7f122a9df105602f2ae97c34cfa80ae032d
jonesbrynn <jonesbrynn1@yahoo.com>
2021-12-16 09:02:01 -0500

    national law exists

--- src/national-law/national-law.component.ts
+++ src/national-law/national-law.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NATLAWS } from '../app/natlaws';
-import {MatToolbarModule} from '@angular/material/toolbar';
-
 
 @Component({
   selector: 'app-national-law',
@@ -9,7 +7,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   styleUrls: ['./national-law.component.css'],
 })
 export class NationalLawComponent implements OnInit {
-  natlaw = NATLAWS;
+  natlaws = NATLAWS;
 
   constructor() {}
 
